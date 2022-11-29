d7847342800f2e6517bf23c503439ecca9165d94
Shehrryar Khan <Sherrykhan2999@gmail.com>
2022-11-29 17:31:25 +0500

    Array map Es6 error fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let num1 = [
     [5,3,4],
-    [11,3,5],
+    [2,3,5],
     [6,7,4]
 ]
 
 let num2 = [
     [2,3,4],
-    [1,12,5],
-    [6,15,4]
+    [1,4,2],
+    [3,2,4]
 ]
 
 let result = [
@@ -30,7 +30,11 @@ let Add =()=>{
     
     document.querySelector("#screen").innerHTML = `[${result[0]}], <br> [${result[1]}],<br> [${result[2]}]`; 
 }
+let Clear =()=>{
+    document.querySelector("#screen").innerHTML = " "
+    return;
 
+}
 
 
 
